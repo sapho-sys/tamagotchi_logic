c7c432bd7ed118c86f664b96168525af8839a9e3
Nkunzi <saphonkunzi@gmail.com>
2023-03-23 14:15:10 +0200

    (feat): Sleep and Dance methods passing

--- tests/dance.test.js
+++ tests/dance.test.js
@@ -1,17 +1,16 @@
 describe('Testing the Tamagotchi Dance Moves', () =>{
-    it('When the Pet Dances, the ENERGY bar should decrease', ()=>{
+    it('When the Pet Dances, the ENERGY bar should decrease to 90%', ()=>{
         let pet_moves = PetDance();
         pet_moves.SimulateDance();
         assert.equal(90, pet_moves.Energy());
     });
-    it('When the Pet Dances for the second time, the ENERGY bar should decrease to 80', ()=>{
+    it('When the Pet Dances for the second time, the ENERGY bar should decrease to 80%', ()=>{
         let pet_moves = PetDance();
         pet_moves.SimulateDance();
         pet_moves.SimulateDance();
         assert.equal(80, pet_moves.Energy());
     });
-
-    it('When the Pet Dances for the second time, the ENERGY bar should decrease to 70', ()=>{
+    it('When the Pet Dances for the second time, the ENERGY bar should decrease to 70%', ()=>{
         let pet_moves = PetDance();
         pet_moves.SimulateDance();
         pet_moves.SimulateDance();
@@ -32,6 +31,5 @@ describe('Testing the Tamagotchi Dance Moves', () =>{
         pet_moves.SimulateDance();
         pet_moves.SimulateDance();
         assert.equal('I want to sleep, I have ran out of energy...', pet_moves.Message());
-
-    })
+    });
 });
