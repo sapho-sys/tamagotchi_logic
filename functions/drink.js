d46d7a9970a2c1d4c20ba87392e1428866a71d30
Nkunzi <saphonkunzi@gmail.com>
2023-03-23 17:13:32 +0200

    (feat): Will continue tomorrow

--- functions/drink.js
+++ functions/drink.js
@@ -1,5 +1,15 @@
 const PetDrinks = () =>{
     let PetEnergy = PetDance();
     let Energy = PetEnergy.Energy();
+    let Thirst = 100;
+    let drink = 5;
+
+    const Thirsty = () =>{
+        return (Thirst/100) * 100;
+    };
+
+    return {
+        Thirsty
+    }
 
 };
